7676883367826632285356d3f121d1ae2e831fa4
Patrik Wagner <wagnerp@starschema.net>
2020-08-09 14:06:57 +0200

    moved exporting

--- holiday-planner/app.js
+++ holiday-planner/app.js
@@ -19,9 +19,10 @@ const routeReducer = (optimalRoute, [destination, desDependency]) => {
   return optimalRoute;
 };
 
-module.exports = generateRoute = (destinations) =>
+const generateRoute = (destinations) =>
   (destinations || []).map(transformInput).reduce(routeReducer, []).join('');
-
 
 const userInput = process.argv.slice(2);
 userInput && console.log(generateRoute(userInput));
+
+module.exports = generateRoute;
